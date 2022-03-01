4e8da7d9a1922f61eddbcc954ba41da5d153d1c4
Samuel Iatarola <samueliatarola@gmail.com>
2022-02-28 21:19:46 -0300

    Feat: Timer component functional

---
+++ src/components/timer/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class='containerTimer'>
+    <div id='timerSeconds'>
+      <Title :text="seconds"></Title>
+      <TextContent text="seconds"></TextContent>
+    </div>
+  </div>
+</template>
+
+<script>
+import TextContent from '@/components/textContent'
+import Title from '@/components/title/index.vue'
+import router from '@/router/index.js'
+export default {
+  name: 'Timer',
+  components: {
+    TextContent,
+    Title
+  },
+  data() {
+    return {
+      seconds: '',
+      timerShow: true
+    }
+  },
+  methods: {
+    timerUpdate(){
+      if(this.timerShow){
+        this.seconds --;
+        if(this.seconds == 0){
+          this.stop()
+        }
+      }
+    },
+    stop(){
+      this.timerShow = false;
+      router.push({name:'Login'})
+    }
+  },
+  created(){
+    this.seconds = 600
+    setInterval(() => {
+      this.timerUpdate()
+    }, 1000)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
