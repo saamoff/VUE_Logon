5a630a876d1b25f2c7c0b972a2f0dbdf62b1a5e7
Samuel Iatarola <samueliatarola@gmail.com>
2022-03-01 13:23:41 -0300

    Feat: Loginpage created and assembled

---
+++ src/views/Login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login">
+    <LoginLeft></LoginLeft>
+    <LoginRight></LoginRight>
+  </div>
+</template>
+
+<script>
+import LoginRight from '@/containers/loginContainerRight/index.vue'
+import LoginLeft from '@/containers/loginContainerLeft/index.vue'
+export default {
+  name: 'Login',
+  components: {
+    LoginRight,
+    LoginLeft
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
