790fe4d3cce90d40f2c7ab0552244676d6eca49b
Samuel Iatarola <samueliatarola@gmail.com>
2022-03-01 13:17:59 -0300

    Feat: Assembled homepage

---
+++ src/views/Home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Header></Header>
+    <ContentHome></ContentHome>
+    <HomeFooter></HomeFooter>
+  </div>
+</template>
+
+<script>
+import Header from '@/containers/header/index.vue'
+import ContentHome from '@/containers/contentHome/index.vue'
+import HomeFooter from '@/containers/homeFooter/index.vue'
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    ContentHome,
+    HomeFooter
+  }
+}
+</script>
