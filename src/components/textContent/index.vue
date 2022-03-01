f28b670284b00212fd34488f0acaf00f9a44055f
Samuel Iatarola <samueliatarola@gmail.com>
2022-02-28 21:15:30 -0300

    Feat: Textcontent and title components functional

---
+++ src/components/textContent/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <p :class="{tempClass:temperature, cityClass:city, pContentTitle:pContentHome1, pContentLast:pContentHome2, pContentFooter:pFooter, pTimerClass:pTimer, pLoginClass:pLogin}">{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextContent',
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: 'Sample Text.',
+    },
+    temp: {
+      default: false
+    },
+    city: {
+      default: false
+    },
+    pContentHome1: {
+      default: false
+    },
+    pContentHome2: {
+      default: false
+    },
+    pFooter: {
+      default: false
+    },
+    pTimer: {
+      default: false
+    },
+    pLogin: {
+      default: false
+    }
+  },
+  data(){
+    return {
+      temperature: this.temp
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
---
+++ src/components/title/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1 v-if="control === '1'" :class="{contentHomeTitle:contentHome1, contentHome:contentHome2, clockClass:clock, loginTitle:titleLogin, detailsLogin:titleDetails}">{{ text }}</h1>
+  <h2 v-else-if="control === '2'" :class="{contentHomeTitle:contentHome1, contentHome:contentHome2, clockClass:clock, loginTitle:titleLogin, detailsLogin:titleDetails}">{{ text }}</h2>
+  <h3 v-else-if="control === '3'" :class="{contentHomeTitle:contentHome1, contentHome:contentHome2, clockClass:clock, loginTitle:titleLogin, detailsLogin:titleDetails}">{{ text }}</h3>
+  <h4 v-else-if="control === '4'" :class="{contentHomeTitle:contentHome1, contentHome:contentHome2, clockClass:clock, loginTitle:titleLogin, detailsLogin:titleDetails}">{{ text }}</h4>
+  <h5 v-else-if="control === '5'" :class="{contentHomeTitle:contentHome1, contentHome:contentHome2, clockClass:clock, loginTitle:titleLogin, detailsLogin:titleDetails}">{{ text }}</h5>
+  <h6 v-else-if="control === '6'" :class="{contentHomeTitle:contentHome1, contentHome:contentHome2, clockClass:clock, loginTitle:titleLogin, detailsLogin:titleDetails}">{{ text }}</h6>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: 'Sample Title',
+    },
+    control: {
+      type: String,
+      required: true,
+      default: '1',
+    },
+    clock: {
+      default: false
+    },
+    contentHome1: {
+      default: false
+    },
+    contentHome2: {
+      default: false
+    },
+    titleLogin: {
+      default: false
+    },
+    titleDetails: {
+      default: false
+    }
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+@import './index.scss';
+</style>
