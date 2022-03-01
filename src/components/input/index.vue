1ddf600d373df977ee8668733f0235bad3e2a733
Samuel Iatarola <samueliatarola@gmail.com>
2022-02-28 21:22:46 -0300

    Feat: Input component functional

---
+++ src/components/input/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="{inputTextClass:inputText, inputSubmitClass:inputSubmit}" id="inputLogin">
+    <input :type="typeInput" required :placeholder="placeHolder" :value="innerHTML">
+    <img v-if="icon" :src="require(`@/assets/images/${iconID}.png`)" alt="Icon">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputLogin',
+  props: {
+    typeInput: {
+      type: String,
+      default: 'text'
+    },
+    placeHolder: {
+      type: String,
+      default: 'Sample Text'
+    },
+    innerHTML: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    inputText: {
+      default: false
+    },
+    inputSubmit: {
+      default: false
+    }
+  },
+  data(){
+    return {
+      iconID: this.icon
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
